4f5e204c1d4be8c2785094aa03488d30378653f2
jrag-dev <josealvarado2117@gmail.com>
2025-03-08 01:07:50 -0400

    refactor(layout): refactor layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuth.jsx'
 
 
 const AuthLayout = () => {
+  const { auth } = useAuthContext();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (auth._id) {
+      navigate('/admin')
+    }
+    setLoading(false)
+  }, [auth])
+
+  if (loading) return <p>Cargando...</p>
+
   return (
     <>
       <main className="w-[90%] container mx-auto grid grid-cols-1 md:grid-cols-2 md:gap-14 place-items-center min-h-svh">
